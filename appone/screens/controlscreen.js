5586ea8535ae8cd84c4020e98edec7ad107436e5
SirajKarim <msirajakarim437@gmail.com>
2020-11-05 14:30:34 +0500

    add

--- appone/screens/controlscreen.js
+++ appone/screens/controlscreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet,View,Text } from 'react-native';
 import { Card, Button,Title,Paragraph  } from 'react-native-paper';
 import TopHeader from '../components/header';
+import SwitchBtn from '../components/switch';
 
 
 function ControlScreen() {
@@ -9,20 +10,47 @@ function ControlScreen() {
          <View style = {styles.mybg} >
              <TopHeader heading = "Room Controls"/>
              <View style = {styles.container}>
-                 <View style={styles.tile}></View>
-                 <View style={styles.tile}></View>
-                 <View style={styles.tile}></View>
+                 <View style={styles.tile}>
+                     <SwitchBtn />
+                 </View>
+                 <View style={styles.tile}>
+                 <SwitchBtn />
+                 </View>
+              
              </View>
              <View style = {styles.container}>
-                 <View style={styles.tile}></View>
-                 <View style={styles.tile}></View>
-                 <View style={styles.tile}></View>
+                 <View style={styles.tile}>
+                 <SwitchBtn />
+                 </View>
+                 <View style={styles.tile}>
+                 <SwitchBtn />
+                 </View>
+               
              </View>
              <View style = {styles.container}>
-                 <View style={styles.tile}></View>
+                 <View style={styles.tile}>
+                 <SwitchBtn />
+                 </View>
+                 <View style={styles.tile}>
+                 <SwitchBtn />
+                 </View>
+               
+             </View>
+             <View style = {styles.container}>
+                 <View style={styles.tile}>
+                 </View>
+                 <View style={styles.tile}>
                 
+                 </View>
+               
              </View>
-             
+             <View style = {styles.container}>
+                 <View style={styles.rgbtile}>
+                 </View>
+                
+               
+             </View>
+            
          </View>
     );
 }
@@ -33,20 +61,30 @@ const styles = StyleSheet.create({
         flex : 1,
     },
     container: {
-        flex: 0.3,
+        flex: 0.185,
         flexWrap: 'wrap',
         flexDirection: 'row',
         // alignItems:'center',
         justifyContent: 'space-evenly',
+        // backgroundColor:'grey'
       
 
         
     },
     tile: {
-        height: '65%',
-        width:'30%',
+        height: '80%',
+        width:'45%',
         backgroundColor:'lightblue',
-        marginTop: '5%'
+        justifyContent: 'center',
+        marginTop: '4%'
+
+    },
+    rgbtile: {
+        height: '100%',
+        width:'92%',
+        backgroundColor:'lightblue',
+        justifyContent: 'center',
+        marginTop: '4%'
 
     }
 });
